ac62f7d10c03eb1747da40d3fa10262add61b652
giochavez <54290463+giochavez@users.noreply.github.com>
2020-11-01 20:33:29 -0600

    Update lista.c

--- lista.c
+++ lista.c
@@ -26,9 +26,11 @@ void imprimirLista(Lista *pLista){
     if(pLista->inicio== NULL){ // revisamos si la lista está o no vacía
         printf("La lista esta vac%ca", 161);
     }else{ //si la lista esta vacia
-        while(pLista->inicio != NULL){ // y mientras lista sea diferente a nulo 
-        printf("Nombre: %s \nCantidad en stock: %d\nPrecio unitario: %0.2lf\n\n", pLista->inicio->informacion);
+        while(pLista->inicio != NULL){ // y mientras lista sea diferente a nulo
+        printf("Nombre: %s \n", pLista->inicio->informacion.nombre); //Imprimimos el nombre que esta apuntado en informacion en la propiedad nombre
+        printf("Cantidad en stock: %d\n", pLista->inicio->informacion.cantidad); //Imprimimos la cantidad en stock que esta en nuestro "inicio" en nuestra propiedad informacion.cantidad
+        printf("Precio unitario: %0.2lf\n\n", pLista->inicio->informacion.precio); //Imprimimos el precio que esta guardado en la propiedad informacion.precio
         pLista->inicio = pLista->inicio->siguiente; //condicionamos a que inicio apunte a su siguiente (asi hasta null)
-        } //De esa manera aseguramos que el contador vaya navegando atraves de los apuntadores
-    }
+        } //De esa manera aseguramos que el contador vaya navegando atraves de los apuntadores desde su propiedad siguiente hasta que encuentre un NULL
+    } //De no hacer "informacion.propiedad" nos marcará error ya que estamos ocupando apuntadores, y al solo poner %d o %s, nos estamos refiriendo a valores simples y enteros
 }
